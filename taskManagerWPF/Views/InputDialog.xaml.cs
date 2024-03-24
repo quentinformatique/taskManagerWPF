ab80e647b2eab0bb920b3984783fb9742c0feb3c
quentinformatique <quentin161204@gmail.com>
2024-03-24 20:43:12 +0100

    Create InputDialog.xaml.cs

---
+++ taskManagerWPF/Views/InputDialog.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace taskManagerWPF.Views
+{
+    public partial class InputDialog : Window
+    {
+        public string Answer { get; private set; }
+
+        public InputDialog(string title, string question, string defaultAnswer = "")
+        {
+            InitializeComponent();
+
+            Title = title;
+            QuestionTextBlock.Text = question;
+            AnswerTextBox.Text = defaultAnswer;
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            Answer = AnswerTextBox.Text;
+            DialogResult = true;
+        }
+    }
+}
