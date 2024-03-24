bae7870986244951f45c84e5859f0532b6dc4938
quentinformatique <quentin161204@gmail.com>
2024-03-24 19:02:50 +0100

    Create TaskManagerViewModel.cs

---
+++ taskManagerWPF/ViewModels/TaskManagerViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using taskManagerWPF.Models;
+
+namespace taskManagerWPF.ViewModels
+{
+    internal class TaskManagerViewModel
+    {
+        public ObservableCollection<TaskItem> Tasks { get; set; }
+        public ICommand AddTaskCommand { get; set; }
+        public ICommand DeleteTaskCommand { get; set; }
+
+        public TaskManagerViewModel()
+        {
+            Tasks = new ObservableCollection<TaskItem>();
+            AddTaskCommand = new RelayCommand(AddTask);
+            DeleteTaskCommand = new RelayCommand(DeleteTask);
+        }
+
+        private void AddTask(object parameter)
+        {
+            Tasks.Add(new TaskItem("Nouvelle tâche", "description"));
+        }
+
+        private void DeleteTask(object parameter)
+        {
+            // Supprimer la tâche sélectionnée de la liste des tâches
+        }
+    }
+}
