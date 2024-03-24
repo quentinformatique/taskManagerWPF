0d91f5036628e2b89279dbe8f9c8687dbf0bee8e
quentinformatique <quentin161204@gmail.com>
2024-03-24 20:03:41 +0100

    remove delete

--- taskManagerWPF/ViewModels/TaskManagerViewModel.cs
+++ taskManagerWPF/ViewModels/TaskManagerViewModel.cs
@@ -8,23 +8,16 @@ namespace taskManagerWPF.ViewModels
     {
         public ObservableCollection<TaskItem> TaskItems { get; set; }
         public ICommand AddTaskCommand { get; set; }
-        public ICommand DeleteTaskCommand { get; set; }
 
         public TaskManagerViewModel()
         {
             TaskItems = new ObservableCollection<TaskItem>();
             AddTaskCommand = new RelayCommand(AddTask);
-            DeleteTaskCommand = new RelayCommand(DeleteTask);
         }
 
         private void AddTask(object parameter)
         {
             TaskItems.Add(new TaskItem("title"));
-        }
-
-        private void DeleteTask(object parameter)
-        {
-            // Supprimer la tâche sélectionnée de la liste des tâches
         }
     }
 }
