01fe8c82723a08b837a45258eef7689744aa3bd7
quentinformatique <quentin161204@gmail.com>
2024-03-24 18:03:28 +0100

    Update Task.cs

--- taskManagerWPF/Models/Task.cs
+++ taskManagerWPF/Models/Task.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,26 @@ using System.Threading.Tasks;
 
 namespace taskManagerWPF.Models
 {
-    internal class Task
+    class Task
     {
+
+        private String title;
+
+        private String description;
+
+        private bool done;
+
+        public Task(String title, String description) 
+        {
+            this.title = title;
+            this.description = description;
+            done = false;
+        }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool Done { get; set; }
     }
 }
